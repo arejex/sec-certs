100b9d834f6b0a1a35aed366d26f374145cbf804
Jakub Borsky <79481056+jborsky@users.noreply.github.com>
2026-07-18 10:38:47 +0200

    feat: pass fresh argument to extract_data

--- src/sec_certs/sample/protection_profile.py
+++ src/sec_certs/sample/protection_profile.py
@@ -357,7 +357,6 @@ class ProtectionProfile(
         """
         try:
             cert.pdf_data.report_metadata = extract_pdf_metadata(cert.state.report.source_path)
-            cert.state.report.extract_ok = True
         except ValueError:
             cert.state.report.extract_ok = False
         return cert
@@ -369,7 +368,6 @@ class ProtectionProfile(
         """
         try:
             cert.pdf_data.pp_metadata = extract_pdf_metadata(cert.state.pp.source_path)
-            cert.state.pp.extract_ok = True
         except ValueError:
             cert.state.pp.extract_ok = False
 
